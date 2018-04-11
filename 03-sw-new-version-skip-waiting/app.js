3bf9ee7723a8c5b9cd6b134a46589bc603da6f54
Mikhail Istomin <mikhail_istomin@epam.com>
2018-04-11 20:44:12 +0300

    example 3

---
+++ 03-sw-new-version-skip-waiting/app.js
@@ -0,0 +1,26 @@
+var swCurrentVersion;
+const swVersionStorageName = 'ex-3-sw-version';
+
+if ('serviceWorker' in navigator) {
+  swCurrentVersion = window.localStorage.getItem(swVersionStorageName) || 1;
+  currentSwVersionIndicator.innerHTML = swCurrentVersion;
+  navigator.serviceWorker.register(`sw-${swCurrentVersion}.js`)
+  .then(function(reg) {
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function(error) {
+    console.log('Registration failed with ' + error);
+  });
+}
+
+add1.onclick = function(){
+  var img = document.createElement("img");
+  img.setAttribute("src", "snake.jpg");
+  document.body.appendChild(img);
+}
+swChangeVersion.onclick = function(){
+  let newVersion = parseInt(swCurrentVersion) === 1 ? 2 : 1;
+  swVersion = window.localStorage.setItem(swVersionStorageName, newVersion);
+  currentSwVersionIndicator.innerHTML = newVersion;
+}
+
+
